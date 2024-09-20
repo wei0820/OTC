4cff0ab228daddf76969fe1af8b7c9ba4dda6d96
JackPan <jheng.wei0919@gmail.com>
2024-09-20 17:28:10 +0800

    commit

--- app/src/main/java/com/tools/payhelper/AddWechatActivity.java
+++ app/src/main/java/com/tools/payhelper/AddWechatActivity.java
@@ -10,7 +10,6 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
