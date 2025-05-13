385d1d204c9534b8102997750de0a8dcfb4e70cf
JackPan <jheng.wei0919@gmail.com>
2025-05-13 15:46:11 +0800

    commit

--- app/src/main/java/com/tools/payhelper/pay/ui/home/PaymentIsValidData.java
+++ app/src/main/java/com/tools/payhelper/pay/ui/home/PaymentIsValidData.java
@@ -1,9 +1,10 @@
 package com.tools.payhelper.pay.ui.home;
 
 import com.google.gson.annotations.SerializedName;
+import com.tools.payhelper.pay.Constant;
 
 public class PaymentIsValidData {
 
-    @SerializedName("code") public  int code ;
-    @SerializedName("msg") public  String msg ;
+    @SerializedName("code") public  int code = Constant.DefaultValue_Int;
+    @SerializedName("msg") public  String msg = Constant.DefaultValue_String ;
 }
