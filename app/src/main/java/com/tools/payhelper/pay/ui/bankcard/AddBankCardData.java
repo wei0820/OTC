3a520c8f439188fbe4aa8e353359a0161273c01b
JackPan <jheng.wei0919@gmail.com>
2025-05-13 15:48:50 +0800

    commit

--- app/src/main/java/com/tools/payhelper/pay/ui/bankcard/AddBankCardData.java
+++ app/src/main/java/com/tools/payhelper/pay/ui/bankcard/AddBankCardData.java
@@ -1,11 +1,12 @@
 package com.tools.payhelper.pay.ui.bankcard;
 
 import com.google.gson.annotations.SerializedName;
+import com.tools.payhelper.pay.Constant;
 
 public class AddBankCardData {
 
-    @SerializedName("code") public  int code;
-    @SerializedName("msg") public  String msg;
+    @SerializedName("code") public  int code = Constant.DefaultValue_Int;
+    @SerializedName("msg") public  String msg = Constant.DefaultValue_String;
 
 
 }
